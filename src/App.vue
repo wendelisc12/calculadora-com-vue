f7dabf6c26786bd5dcf2e4c87557f22b62ae7f22
Wendel <wendel.isaac12@gmail.com>
2024-07-09 01:46:18 -0300

    criacao de calculadora

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { reactive } from "vue";
+import resultado from "./components/resultado.vue";
+
+
+const values = reactive({
+  primeiroNumero: 0,
+  segundoNumero: 0,
+  operacao: 'adicao',
+  resultado: 0
+})
+
+function calcular(){
+  if(values.operacao === "adicao"){
+    values.resultado = values.primeiroNumero + values.segundoNumero
+  }else if(values.operacao === "subtracao"){
+    values.resultado = values.primeiroNumero - values.segundoNumero
+  }else if(values.operacao === "multiplicacao"){
+    values.resultado = values.primeiroNumero * values.segundoNumero
+  }else if(values.operacao === "divisao"){
+    values.resultado = values.primeiroNumero / values.segundoNumero
+  }
+}
+
+function escolha(op, e){
+  if(op === "primeiro"){
+    values.primeiroNumero = parseInt(e.target.value)
+    calcular()
+  }else if(op === "segundo"){
+    values.segundoNumero = parseInt(e.target.value)
+    calcular()
+  }else if(op === "operacao"){
+    values.operacao = e.target.value
+    calcular()
+  }
+}
+
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="text-center mt-5">
+      <h1>Calculadora</h1>
+    </div>
+
+    <div class="d-flex flex-column mt-4 align-items-center gap-3">
+      <input type="number" class="w-25 form-control" @input="(e) => escolha('primeiro', e)" placeholder="Digite um número">
+      <input type="number" class="w-25 form-control" @input="(e) => escolha('segundo', e)" placeholder="Digite um número">
+      <select class="w-25 form-control" @change="(e) => escolha('operacao', e)" id="">
+        <option value="adicao">Adição</option>
+        <option value="subtracao">Subtração</option>
+        <option value="multiplicacao">Multiplicação</option>
+        <option value="divisao">Divisão</option>
+      </select>
+    </div>
+
+    <div class="d-flex justify-content-center mt-4">
+      <resultado :resultado="values.resultado" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/resultado.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps(['resultado'])
+</script>
+
+<template>
+    <div class="text-center border border-dark rounded p-5">
+        <h1>Resultado:</h1>
+        <h1 class="display-1">{{props.resultado}}</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
